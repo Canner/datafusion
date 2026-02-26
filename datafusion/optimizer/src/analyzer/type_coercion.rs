49432baeb57e8bb95ecaf900ccfc33c8f2ce39c7
Douenergy <103009868+douenergy@users.noreply.github.com>
2026-02-26 14:45:36 +0800

    support Duration type for window framing (#11)
    * duration type
    
    * fix sqllogictest

--- datafusion/optimizer/src/analyzer/type_coercion.rs
+++ datafusion/optimizer/src/analyzer/type_coercion.rs
@@ -730,6 +730,8 @@ fn extract_window_frame_target_type(col_type: &DataType) -> Result<DataType> {
         Ok(DataType::Interval(IntervalUnit::MonthDayNano))
     } else if let DataType::Dictionary(_, value_type) = col_type {
         extract_window_frame_target_type(value_type)
+    } else if let DataType::Duration(_) = col_type {
+        Ok(col_type.clone())
     } else {
         return internal_err!("Cannot run range queries on datatype: {col_type:?}");
     }
